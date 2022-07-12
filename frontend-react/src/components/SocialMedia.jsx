859a8a913a75a867f2403085c8766d4d4ad5e28a
Casper De Keyser <casper.dekeyser@student.ap.be>
2022-07-12 15:07:39 +0200

    skills & experience done, adding some animations and fixing social media icons

--- frontend-react/src/components/SocialMedia.jsx
+++ frontend-react/src/components/SocialMedia.jsx
@@ -1,21 +1,17 @@
 import React from 'react';
-import { BsTwitter, BsInstagram} from 'react-icons/bs'
-import { FaFacebookF } from 'react-icons/fa'
+import { BsLinkedin} from 'react-icons/bs'
 
 const SocialMedia = () => {
   return (
-    <div className='app__social'>
-        <div>
-            <BsTwitter />
-        </div>
-        <div>
-            <FaFacebookF />
-        </div>
-        <div>
-            <BsInstagram />
-        </div>
-    </div>
-  )
+		<div className="app__social">
+			<div>
+				<a href="https://www.linkedin.com/in/casperdekeyser/">
+					{" "}
+					<BsLinkedin />
+				</a>
+			</div>
+		</div>
+	);
 }
 
 export default SocialMedia
