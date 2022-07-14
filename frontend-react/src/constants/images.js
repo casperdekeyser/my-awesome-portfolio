1dc1eac75d845a400d762912216718ff4ebbd3af
casperdekeyser <casper.dekeyser@gmail.com>
2022-07-14 18:30:52 +0200

    test project on mac

--- frontend-react/src/constants/images.js
+++ frontend-react/src/constants/images.js
@@ -13,8 +13,8 @@ import circle from '../assets/circle.svg';
 
 import casper from '../assets/casper-profile.webp';
 import linkedin from '../assets/linkedin-logo.png';
-import belgium from '../assets/belgium.png'
-import logo from '../assets/logo.png'
+import belgium from '../assets/belgium.png';
+import logo from '../assets/logo.png';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
