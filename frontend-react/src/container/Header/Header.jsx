393fd02abdc5dadf7ba28a363fe4109ffc601dc1
Casper De Keyser <casper.dekeyser@student.ap.be>
2022-07-14 15:53:34 +0200

    cleaning up images

--- frontend-react/src/container/Header/Header.jsx
+++ frontend-react/src/container/Header/Header.jsx
@@ -61,7 +61,7 @@ const Header = () => (
 			whileInView={scaleVariants.whileInView}
 			className="app__header-circles"
 		>
-			{[images.flutter, images.redux, images.belgium].map((circle, index) => (
+			{[images.flutter, images.javascript, images.belgium].map((circle, index) => (
 				<div className="circle-cmp app__flex" key={`circle-${index}`}>
 					<img src={circle} alt="profile_bg" />
 				</div>
