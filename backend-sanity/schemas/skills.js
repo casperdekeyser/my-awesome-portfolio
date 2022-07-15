687634ea4c14954139dba0681cb14b520ed1c7c6
Casper De Keyser <casper.dekeyser@student.ap.be>
2022-07-15 16:16:24 +0200

    added hover description for skills instead of work
    removed age bubble

--- backend-sanity/schemas/skills.js
+++ backend-sanity/schemas/skills.js
@@ -9,6 +9,11 @@ export default{
             type:'string'
         },
         {
+            name: 'description',
+            title: 'Description',
+            type: 'string'
+        },
+        {
             name:'bgColor',
             title:'BgColor',
             type:'string'
--- frontend-react/src/container/Skills/Skills.jsx
+++ frontend-react/src/container/Skills/Skills.jsx
@@ -26,24 +26,35 @@ const Skills = () => {
 	return (
 		<>
 			<h2 className="head-text">Skills & Experiences</h2>
-
 			<div className="app__skills-container">
 				<motion.div className="app__skills-list">
 					{skills.map((skill) => (
-						<motion.div
-							whileInView={{ opacity: [0, 1] }}
-							transition={{ duration: 0.5 }}
-							className="app__skills-item app__flex"
-							key={skill.name}
-						>
-							<div
-								className="app__flex"
-								style={{ backgroundColor: skill.bgColor }}
+						<>
+							<motion.div
+								whileInView={{ opacity: [0, 1] }}
+								transition={{ duration: 0.5 }}
+								className="app__skills-item app__flex"
+								data-tip
+								data-for={skill.name}
+								key={skill.name}
 							>
-								<img src={urlFor(skill.icon)} alt={skill.name} />
-							</div>
-							<p className="p-text">{skill.name}</p>
-						</motion.div>
+								<div
+									className="app__flex"
+									style={{ backgroundColor: skill.bgColor }}
+								>
+									<img src={urlFor(skill.icon)} alt={skill.name} />
+								</div>
+								<p className="p-text">{skill.name}</p>
+							</motion.div>
+							<ReactTooltip
+								id={skill.name}
+								effect="solid"
+								arrowColor="#fff"
+								className="skills-tooltip"
+							>
+								{skill.description}
+							</ReactTooltip>
+						</>
 					))}
 				</motion.div>
 				<div className="app__skills-exp">
@@ -59,21 +70,21 @@ const Skills = () => {
 											whileInView={{ opacity: [0, 1] }}
 											transition={{ duration: 0.5 }}
 											className="app__skills-exp-work"
-											data-tip
-											data-for={work.name}
+											// data-tip
+											// data-for={work.name}
 											key={work.name}
 										>
 											<h4 className="bold-text">{work.name}</h4>
 											<p className="p-text">{work.company}</p>
 										</motion.div>
-										<ReactTooltip
+										{/* <ReactTooltip
 											id={work.name}
 											effect="solid"
 											arrowColor="#fff"
 											className="skills-tooltip"
 										>
 											{work.desc}
-										</ReactTooltip>
+										</ReactTooltip> */}
 									</>
 								))}
 							</motion.div>
